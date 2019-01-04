7cdf538cc63943ec68a76993b6010a77e2a3b5fe
Tiago Arrazi <tiago_arrazi98@outlook.com>
2019-01-04 15:23:36 -0200

    Updates

--- Section 13/my_flask_app/app.py
+++ Section 13/my_flask_app/app.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 
 
 app = Flask(__name__)
+posts = {}
 
 
 @app.route('/')
 def home():
-    return 'Hello, world!'
+    return render_template('home.html')
+
+
+@app.route('/post/<int:post_id>')
+def post_it(post_id):
+    post = posts.get(post_id)
+    if not post:  # post will be None if not found; not None -> True
+        return render_template('404.html',
+                               message=f'Post with id {post_id} not found')
+    return render_template('post.html', post=post)
+
+
+@app.route('/post/form')
+def form():
+    return render_template('create.html')
+
+
+@app.route('/post/create', methods=['GET', 'POST'])
+def create():
+    title = request.form.get('title')
+    content = request.form.get('content')
+    post_id = len(posts)
+    posts[post_id] = {'id': post_id, 'title': title, 'content': content}
+
+    return redirect(url_for('post_it', post_id=post_id))
 
 
 if __name__ == '__main__':
